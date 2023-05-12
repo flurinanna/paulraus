69769958697599c4c8bff49d228a448394b79ffd
Benjamin Berger <b3n@b3n.ch>
2023-05-12 21:17:13 +0200

    Tryna fix.

--- src/components/ARPage.js
+++ src/components/ARPage.js
@@ -1,9 +1,32 @@
+import React, { useState } from 'react';
+import MindARViewer from './mindar-viewer';
 import MindARThreeViewer from './mindar-three-viewer';
 
 export default function ARPage({children}) {
-    return <div>
-            <div className="container">
-                <MindARThreeViewer />
+    const [started, setStarted] = useState(null);
+
+    return (
+      <div className="App">
+        <h1>Example React component with <a href="https://github.com/hiukim/mind-ar-js" target="_blank" rel="noreferrer">MindAR</a></h1>
+  
+        <div className="control-buttons">
+          {started === null && <button onClick={() => {setStarted('aframe')}}>Start AFRAME version</button>}
+          {started === null && <button onClick={() => {setStarted('three')}}>Start ThreeJS version</button>}
+          {started !== null && <button onClick={() => {setStarted(null)}}>Stop</button>}
         </div>
-    </div>
+  
+        {started === 'aframe' && (
+          <div className="container">
+            <MindARViewer/>
+            <video></video>
+          </div>
+        )}
+  
+        {started === 'three' && (
+          <div className="container">
+            <MindARThreeViewer />
+          </div>
+        )}
+      </div>
+    );
 }
